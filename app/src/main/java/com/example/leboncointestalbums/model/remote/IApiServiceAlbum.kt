a2bced3531f107805cddd1b73b3090cdeef2ab25
Aymane <aymanerizke199@gmail.com>
2020-12-05 16:27:30 +0100

    create singleton retrofit & end point

--- app/src/main/java/com/example/leboncointestalbums/model/remote/IApiServiceAlbum.kt
+++ app/src/main/java/com/example/leboncointestalbums/model/remote/IApiServiceAlbum.kt
@@ -1,4 +1,15 @@
 package com.example.leboncointestalbums.model.remote
 
-class IApiServiceAlbum {
+import com.example.leboncointestalbums.model.entity.Album
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+
+
+interface IApiServiceAlbum {
+
+    @GET("/img/shared/technical-test.json")
+    fun getAlbums(): Call<List<Album?>?>?
+
 }
--- app/src/main/java/com/example/leboncointestalbums/model/remote/RetrofitFactory.kt
+++ app/src/main/java/com/example/leboncointestalbums/model/remote/RetrofitFactory.kt
@@ -1,4 +1,23 @@
 package com.example.leboncointestalbums.model.remote
 
-class RetrofitFactory {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object  RetrofitFactory {
+
+    private var retrofit: Retrofit
+    private val API_URL = "https://static.leboncoin.fr"
+
+
+    init {
+        retrofit = Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getRetrofit(): Retrofit? {
+        return retrofit
+    }
+
 }
