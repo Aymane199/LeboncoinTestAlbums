df8bbf6d5bfb80d20e5739878e2d68d9ce91d522
Aymane <aymanerizke199@gmail.com>
2020-12-05 17:46:59 +0100

    implement viwmodel

--- app/src/main/java/com/example/leboncointestalbums/viewmodel/listAlbum/ListAlbumViewModel.kt
+++ app/src/main/java/com/example/leboncointestalbums/viewmodel/listAlbum/ListAlbumViewModel.kt
@@ -2,6 +2,22 @@ package com.example.leboncointestalbums.viewmodel.listAlbum
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.leboncointestalbums.model.entity.Album
+import com.example.leboncointestalbums.model.repository.AlbumRepository
 
 class ListAlbumViewModel (application: Application):AndroidViewModel(application){
+
+    private val repository  = AlbumRepository()
+    val showProgress : LiveData<Boolean>
+    val albumList : LiveData<List<Album>>
+
+    init {
+        this.showProgress = repository.showProgress
+        this.albumList = repository.albumList
+    }
+
+    fun getAlbums(){
+        repository.getAlbums()
+    }
 }
