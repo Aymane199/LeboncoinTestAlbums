8223b38d94b1453055b6efe7eb0a2e47f105389f
Aymane <aymanerizke199@gmail.com>
2020-12-05 16:42:22 +0100

    create data class album

--- app/src/main/java/com/example/leboncointestalbums/model/entity/Album.kt
+++ app/src/main/java/com/example/leboncointestalbums/model/entity/Album.kt
@@ -1,4 +1,10 @@
 package com.example.leboncointestalbums.model.entity
 
-class Album {
-}
+
+data class Album (
+    val albumId: Int,
+    val id: Int,
+    val thumbnailUrl: String,
+    val title: String,
+    val url: String
+)
